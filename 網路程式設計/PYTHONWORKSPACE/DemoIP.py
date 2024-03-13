e2ea38305934b8019ac2dc7704a9493de3e8704b
lcw900914 <lcw900914@gmail.com>
2024-03-13 21:35:43 +0800

    新增網路程設資料夾

--- 網路程式設計/PYTHONWORKSPACE/DemoIP.py
+++ 網路程式設計/PYTHONWORKSPACE/DemoIP.py
@@ -1,16 +1,11 @@
 import socket
-
-print("Hello Python")
+import requests
 
 hostname = socket.gethostname() # 拿到主機名稱
 print(hostname)
 ip = socket.gethostbyname(hostname) # 利用主機名稱拿到對應私有IP
 print(ip)
 
-
-# 取得Colab對外連線的NAT公有IP
-
-import requests
-
+# 取得對外連線的NAT公有IP
 p_ip = requests.get('https://api.ipify.org/').text
 print(p_ip)
